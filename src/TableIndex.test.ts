49ab1b708c4f157edcbee73d5555d26a405d8ffc
Moshe Simantov <ms@development.co.il>
2020-06-28 18:24:33 +0300

    add tests for table index

--- src/TableIndex.test.ts
+++ src/TableIndex.test.ts
@@ -1,6 +1,6 @@
 import 'mocha';
 import { assert } from 'chai';
-import { TableKey } from './TableIndex';
+import TableIndex, { TableKey } from './TableIndex';
 
 interface DemoItem {
   Id: string;
@@ -22,6 +22,80 @@ describe('TableIndex', () => {
     assert.ok<TableKey<DemoItem, 'Id', 'Ver'>>({
       Id: 'foo',
       Ver: 1,
+    });
+  });
+
+  describe('.scan()', () => {
+    it('Basic use case', () => {
+      const table = new TableIndex<DemoItem, 'Id', 'Ver'>({
+        tableName: 'MyTable',
+        primaryKey: 'Id',
+        sortKey: 'Ver',
+      });
+
+      assert.deepEqual(
+        table
+          .scan()
+          .filter((cn) => cn.nq('hidden', true))
+          .select('ALL_ATTRIBUTES')
+          .limit(20)
+          .startKey({ Id: '111', Ver: 5 })
+          .serialize(),
+        {
+          TableName: 'MyTable',
+          FilterExpression: '#hidden <> :hidden',
+          IndexName: undefined,
+          ExpressionAttributeNames: {
+            '#hidden': 'hidden',
+          },
+          ExpressionAttributeValues: {
+            ':hidden': true,
+          },
+          ProjectionExpression: undefined,
+          Select: 'ALL_ATTRIBUTES',
+          Limit: 20,
+          ExclusiveStartKey: {
+            Id: '111',
+            Ver: 5,
+          },
+        },
+      );
+    });
+  });
+
+  describe('.query()', () => {
+    it('Basic use case', () => {
+      const table = new TableIndex<DemoItem, 'Id', 'Ver'>({
+        tableName: 'MyTable',
+        primaryKey: 'Id',
+        sortKey: 'Ver',
+      });
+
+      assert.deepEqual(
+        table
+          .query()
+          .keyCondition((cn) => cn.eq('Id', '123').and((cn) => cn.gt('Ver', 5)))
+          .project({ inner: 1 })
+          .reverseIndex()
+          .serialize(),
+        {
+          TableName: 'MyTable',
+          KeyConditionExpression: '#Id = :Id AND #Ver > :Ver',
+          IndexName: undefined,
+          ExpressionAttributeNames: {
+            '#Id': 'Id',
+            '#Ver': 'Ver',
+            '#inner': 'inner',
+          },
+          ExpressionAttributeValues: {
+            ':Id': '123',
+            ':Ver': 5,
+          },
+          ProjectionExpression: '#inner',
+          FilterExpression: undefined,
+          ScanIndexForward: false,
+        },
+      );
     });
   });
 });
