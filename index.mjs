a99f1a2ec0d3c4e51d3aaa4c9ab4bd026d9e0306
Moshe Simantov <ms@development.co.il>
2021-05-27 17:04:56 +0300

    support for es-modules

---
+++ index.mjs
@@ -0,0 +1,4 @@
+import { Table } from "./lib/index.js";
+export * from './lib/index.js';
+
+export default Table;
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import Table from './Table';
 import TableIndex from './TableIndex';
 
 export default Table;
-export { TableIndex };
+export { Table, TableIndex };
+
 export * from './expressions';
 export * from './queries';
 export * from './helpers/sets';
