db3310c210344bd8811a61e92d2de708e13072d3
Moshe Simantov <me@moshe.io>
2023-01-18 12:01:30 +0200

    fix tests

--- src/queries/UpdateQuery.test.ts
+++ src/queries/UpdateQuery.test.ts
@@ -1,12 +1,17 @@
 import UpdateQuery from './UpdateQuery';
 
 describe('queries/UpdateQuery', () => {
+  const dummyClient = { update: () => null } as never;
+
   describe('.set()', () => {
     it('Basic Usage', () => {
       const query = new UpdateQuery<
         { id: number; foo: { bar: string }; baz: number },
         { id: number }
-      >({} as never, { TableName: 'Test', Key: { id: 123 } });
+      >(dummyClient, {
+        TableName: 'Test',
+        Key: { id: 123 },
+      });
 
       query.set('foo', { bar: 'baz' });
       query.set(['baz'], 456);
@@ -16,7 +21,10 @@ describe('queries/UpdateQuery', () => {
       const query = new UpdateQuery<
         { id: number; foo?: { bar: string }; baz: number },
         { id: number }
-      >({} as never, { TableName: 'Test', Key: { id: 123 } });
+      >(dummyClient, {
+        TableName: 'Test',
+        Key: { id: 123 },
+      });
 
       query.set(['foo', 'bar'], 'ddd');
     });
