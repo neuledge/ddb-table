d34ce6f2e334415a0de52fdb0b1b02c45de15211
Moshe Simantov <me@moshe.io>
2023-02-08 14:01:11 +0200

    set undefined remove property

--- src/queries/UpdateQuery.test.ts
+++ src/queries/UpdateQuery.test.ts
@@ -1,4 +1,5 @@
 import UpdateQuery from './UpdateQuery';
+import { assert } from 'chai';
 
 describe('queries/UpdateQuery', () => {
   const dummyClient = { update: () => null } as never;
@@ -15,6 +16,14 @@ describe('queries/UpdateQuery', () => {
 
       query.set('foo', { bar: 'baz' });
       query.set(['baz'], 456);
+
+      assert.deepEqual(query.serialize(), {
+        TableName: 'Test',
+        Key: { id: 123 },
+        UpdateExpression: 'SET #foo = :foo, #baz = :baz',
+        ExpressionAttributeNames: { '#foo': 'foo', '#baz': 'baz' },
+        ExpressionAttributeValues: { ':foo': { bar: 'baz' }, ':baz': 456 },
+      });
     });
 
     it('should support partial deep objects', () => {
@@ -27,6 +36,33 @@ describe('queries/UpdateQuery', () => {
       });
 
       query.set(['foo', 'bar'], 'ddd');
+
+      assert.deepEqual(query.serialize(), {
+        TableName: 'Test',
+        Key: { id: 123 },
+        UpdateExpression: 'SET #foo.#bar = :bar',
+        ExpressionAttributeNames: { '#foo': 'foo', '#bar': 'bar' },
+        ExpressionAttributeValues: { ':bar': 'ddd' },
+      });
+    });
+
+    it('should remove optional properties', () => {
+      const query = new UpdateQuery<
+        { id: number; foo?: { bar: string }; baz: number },
+        { id: number }
+      >(dummyClient, {
+        TableName: 'Test',
+        Key: { id: 123 },
+      });
+
+      query.set('foo', undefined);
+
+      assert.deepEqual(query.serialize(), {
+        TableName: 'Test',
+        Key: { id: 123 },
+        UpdateExpression: 'REMOVE #foo',
+        ExpressionAttributeNames: { '#foo': 'foo' },
+      });
     });
   });
 });
